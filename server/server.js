c75836783305d6eed83bd6ed1aa8adaaa22686d6
itsfarhankhan28 <famannat@gmail.com>
2025-08-21 21:23:23 +0530

    changed cors configuration

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ["http://localhost:5173", "https://freelancer-crm-khaki.vercel.app/"],
+  origin: ["http://localhost:5173", "https://freelancer-crm-omega.vercel.app/"],
   credentials: true
 }));
 app.use(express.json());
