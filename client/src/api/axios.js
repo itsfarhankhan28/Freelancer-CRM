7e525f62513b3350dea0bbbf2a7fa0c932976afc
itsfarhankhan28 <famannat@gmail.com>
2025-08-21 21:20:58 +0530

    changed axios.js

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create({
   baseURL: import.meta.env.MODE === "development"
     ? "/api" 
-    : "https://freelancer-crm-backend.onrender.com/api",
+    : "https://freelancer-crm-backend.onrender.com",
 });
 
 export default api;
