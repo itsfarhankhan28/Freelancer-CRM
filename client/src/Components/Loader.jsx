cf88e5b49d6096cb009a12d7d7338bd53850f98e
itsfarhankhan28 <famannat@gmail.com>
2025-08-24 23:44:39 +0530

    new loader UI

--- client/src/Components/Loader.jsx
+++ client/src/Components/Loader.jsx
@@ -1,58 +1,26 @@
 import React from "react";
-import { Card, CardContent, Skeleton, Box, Typography } from "@mui/material";
+import { Card, CardContent, Skeleton, Box, Button } from "@mui/material";
 
-const Loader = () => {
+const ClientProfileSkeleton = () => {
   return (
-    <Card
-      sx={{
-        maxWidth: 600,
-        margin: "auto",
-        mt: 5,
-        borderRadius: 3,
-        boxShadow: 3,
-      }}
-    >
-      <CardContent>
-        {/* Profile Section */}
-        <Box display="flex" alignItems="center" mb={2}>
-          <Skeleton variant="circular" width={60} height={60} />
-          <Box ml={2} flex={1}>
-            <Skeleton variant="text" width="50%" height={30} />
-            <Skeleton variant="text" width="30%" height={20} />
-          </Box>
-        </Box>
+    <Card sx={{ maxWidth: 400, p: 2, borderRadius: 3, boxShadow: 3 }}>
+      <CardContent sx={{ display: "flex", flexDirection: "column", alignItems: "center", gap: 2 }}>
+        {/* Profile Picture */}
+        <Skeleton variant="circular" width={80} height={80} />
 
-        {/* Client Info */}
-        <Box mb={2}>
-          <Skeleton variant="text" width="80%" height={20} />
-          <Skeleton variant="text" width="60%" height={20} />
-          <Skeleton variant="text" width="40%" height={20} />
-        </Box>
+        {/* Name */}
+        <Skeleton variant="text" width="60%" height={30} />
 
-        {/* Project Section Title */}
-        <Typography variant="h6" mb={1}>
-          <Skeleton width="40%" />
-        </Typography>
+        {/* Email */}
+        <Skeleton variant="text" width="80%" height={20} />
 
-        {/* Scrollable Projects Section */}
-        <Box
-          sx={{
-            maxHeight: 150,
-            overflowY: "auto",
-            border: "1px solid #ddd",
-            borderRadius: 2,
-            p: 1,
-          }}
-        >
-          {[...Array(4)].map((_, index) => (
-            <Box key={index} mb={1}>
-              <Skeleton variant="rectangular" width="100%" height={40} />
-            </Box>
-          ))}
+        {/* Button */}
+        <Box sx={{ width: "100%", display: "flex", justifyContent: "center", mt: 2 }}>
+          <Skeleton variant="rectangular" width={120} height={40} sx={{ borderRadius: 2 }} />
         </Box>
       </CardContent>
     </Card>
   );
 };
 
-export default Loader;
+export default ClientProfileSkeleton;
