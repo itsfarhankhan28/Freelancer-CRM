461c37aeaf5ca5adfc73aa496eebfb6bd074e680
itsfarhankhan28 <famannat@gmail.com>
2025-08-26 20:34:49 +0530

    changed user schema

--- server/models/User.js
+++ server/models/User.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema(
   {
     name: { type: String, trim: true, required: true },
     email: { type: String, trim: true, required: true, unique: true, lowercase: true },
-    passwordHash: { type: String, required: true }
+    password: { type: String, required: true }
   },
   { timestamps: true }
 );
